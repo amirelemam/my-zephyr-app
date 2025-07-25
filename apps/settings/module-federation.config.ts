c2c4e3958bfc4b6c51c36cdf2568441cd89a2168
Amir Elemam <amirelemam@gmail.com>
2025-07-25 16:43:56 -0300

    first commit

--- apps/settings/module-federation.config.ts
+++ apps/settings/module-federation.config.ts
@@ -1,13 +1,17 @@
-import type { ModuleFederationPluginOptions } from "@rspack/core";
+import type { ModuleFederationPluginOptions } from '@rspack/core';
+
+const isDev = process.env.NODE_ENV === 'development';
 
 export const mfConfig: ModuleFederationPluginOptions = {
-  name: "turbo_settings",
-  filename: "remoteEntry.js",
+  name: 'turbo_settings',
+  filename: 'remoteEntry.js',
   exposes: {
-    "./RemoteEntry": "./src/RemoteEntry.tsx"
+    './RemoteEntry': './src/RemoteEntry.tsx',
   },
   remotes: {
-    turbo_host: "turbo_host@http://localhost:3000/remoteEntry.js"
+    turbo_host: isDev
+      ? 'turbo_host@http://localhost:3000/remoteEntry.js'
+      : 'turbo_host@https://turbo-host.zephyr.run/remoteEntry.js',
   },
-  shared: ["react", "react-dom", "react-router"]
+  shared: ['react', 'react-dom', 'react-router'],
 };
